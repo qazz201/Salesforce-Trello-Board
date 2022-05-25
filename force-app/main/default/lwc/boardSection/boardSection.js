471564ea8fe071716a15c63d9acbbae79b6ffcbe
Vladyslav Kravchuk <vladyslav.kravchuk@customertimes.com>
2022-05-25 19:24:55 +0300

    Added drop position

--- force-app/main/default/lwc/boardSection/boardSection.js
+++ force-app/main/default/lwc/boardSection/boardSection.js
@@ -3,16 +3,19 @@ import {LightningElement, api} from 'lwc';
 import {uniqueId, isEmpty, isNotEmpty} from 'c/commons'
 
 const DRAG_ELEMENT_ID = 'dragElementId';
+const CLASS_DRAGGED = 'dragged';
+const HORIZONTAL_SEPARATOR_TAG = 'hr';
+
 
 export default class BoardSection extends LightningElement {
     iterate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    // dropAreas;
+    dropAreas;
     someUniqueId;
 
-    // renderedCallback() {
-    //     this.dropAreas = this.template.querySelectorAll('.drop-area');
-    // }
+    renderedCallback() {
+        this.dropAreas = this.template.querySelectorAll('.drop-area');
+    }
 
     get getUniqueId() {
         this.someUniqueId = uniqueId();
@@ -26,34 +29,57 @@ export default class BoardSection extends LightningElement {
 
     allowDrop(event) {
         if (!event.dataTransfer.types.includes(DRAG_ELEMENT_ID.toLowerCase())) return;
+
         event.preventDefault();
+        this.removeHorizontalSeparatorOnDrag([...this.dropAreas]);
+
+        const itemToInsertBefore = this.getItemToInsertBefore([...event.currentTarget.querySelectorAll('.drag-item')], event.clientY);
+        event.currentTarget.insertBefore(document.createElement(HORIZONTAL_SEPARATOR_TAG), itemToInsertBefore);
     }
+
 
     handleDragStart(event) {
         event.dataTransfer.setData(DRAG_ELEMENT_ID, event.target.dataset.id);
+        event.currentTarget.classList.add(CLASS_DRAGGED);
+    }
+
+    handleDragEnd() {
+        this.removeHorizontalSeparatorOnDrag(this.dropAreas);
+        this.dropAreas.forEach(area => area.querySelector(`.${CLASS_DRAGGED}`)?.classList.remove(CLASS_DRAGGED));
     }
 
     handleDrop(event) {
         event.preventDefault();
+
         const dropItemId = event.dataTransfer.getData(DRAG_ELEMENT_ID);
         const draggedElement = this.getItemById(dropItemId);
 
         if (isEmpty(draggedElement)) return;
+        const itemToInsertBefore = this.getItemToInsertBefore([...event.currentTarget.querySelectorAll('.drag-item')], event.clientY);
 
-        const dragItems = [...event.currentTarget.querySelectorAll('.drag-item')];
-        let isItemInserted = false;
+        if (itemToInsertBefore) {
+            event.currentTarget.insertBefore(draggedElement, itemToInsertBefore);
+        } else {
+            event.currentTarget.appendChild(draggedElement);
+        }
+    }
+
+    removeHorizontalSeparatorOnDrag(dropSections = []) {
+        dropSections.forEach(section => {
+            section.querySelectorAll(HORIZONTAL_SEPARATOR_TAG).forEach(elem => elem.remove());
+        });
+    }
+
+    getItemToInsertBefore(dragItems = [], clientY = 0) {
+        if (isEmpty(dragItems)) return;
 
         for (const item of dragItems) {
             const rect = item.getBoundingClientRect();
             const cord = rect.y + rect.height / 2;
 
-            if (cord >= event.clientY) {
-                event.currentTarget.insertBefore(draggedElement, item);
-                isItemInserted = true;
-                break;
-            }
+            if (cord >= clientY) return item
         }
 
-        if (!isItemInserted) event.currentTarget.appendChild(draggedElement);
+        return;
     }
 }
