898dcac80006a02d518ab6f25e53d6dd7f96531c
Vladyslav Kravchuk <vladyslav.kravchuk@customertimes.com>
2022-05-25 16:46:19 +0300

    Added drop functionality

--- force-app/main/default/lwc/boardSection/boardSection.js
+++ force-app/main/default/lwc/boardSection/boardSection.js
@@ -2,83 +2,58 @@ import {LightningElement, api} from 'lwc';
 
 import {uniqueId, isEmpty, isNotEmpty} from 'c/commons'
 
+const DRAG_ELEMENT_ID = 'dragElementId';
+
 export default class BoardSection extends LightningElement {
     iterate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-    dropAreas;
-
-    // sectionId = uniqueId();
-    // itemId = uniqueId();
+    // dropAreas;
     someUniqueId;
 
-    renderedCallback() {
-        this.dropAreas = this.template.querySelectorAll('.drop-area');
-    }
-
-
-    getItemById(itemId = '') {
-        if (isEmpty(itemId)) return;
-        return this.template.querySelector(`[data-id="${itemId}"]`);
-    }
-
-    //
-    // @api
-    // addItem(itemToAdd) {
-    //     this.dropArea.appendChild(itemToAdd);
+    // renderedCallback() {
+    //     this.dropAreas = this.template.querySelectorAll('.drop-area');
     // }
-
-    addItem(itemToAdd) {
-        this.dropArea.appendChild(itemToAdd);
-    }
 
     get getUniqueId() {
         this.someUniqueId = uniqueId();
         return this.someUniqueId;
     }
 
-    allowDrop(event) {
-        event.preventDefault();
-        const html = event.dataTransfer.types.includes('dragelementid');
-        // console.log('DROP TARGET__', event)
+    getItemById(itemId = '') {
+        if (isEmpty(itemId)) return;
+        return this.template.querySelector(`[data-id="${itemId}"]`);
+    }
 
-        if (html) {
-            event.preventDefault();
-        }
+    allowDrop(event) {
+        if (!event.dataTransfer.types.includes(DRAG_ELEMENT_ID.toLowerCase())) return;
+        event.preventDefault();
     }
 
     handleDragStart(event) {
-        console.log('Start_ ITEM ID', event.target.dataset.id)
-        event.dataTransfer.setData('dragElementId', event.target.dataset.id);
+        event.dataTransfer.setData(DRAG_ELEMENT_ID, event.target.dataset.id);
     }
 
-    // handleDrop(event) {
-    //     console.log('DROP')
-    //     event.preventDefault();
-    //     const dropItemId = event.dataTransfer.getData('dragElementId');
-    //     const dropSectionId = this.dropArea.getAttribute('data-id');
-    //
-    //     let div = document.createElement('div');
-    //     div.innerHTML = `
-    //                     SECTION ID: <b>${dropSectionId}</b> <br/>
-    //                     ITEM ID: <b>${dropItemId}</b>
-    //                     <br/><br/>`;
-    //
-    //     this.template.querySelector('.drop-area').appendChild(div);
-    //
-    //     this.dispatchEvent(new CustomEvent('dropelement', {detail: {dropItemId, dropSectionId}}));
-    //
-    // }
-
     handleDrop(event) {
-        try {
+        event.preventDefault();
+        const dropItemId = event.dataTransfer.getData(DRAG_ELEMENT_ID);
+        const draggedElement = this.getItemById(dropItemId);
 
-            event.preventDefault();
-            const dropItemId = event.dataTransfer.getData('dragElementId');
-            const draggedElement = this.template.querySelector(`[data-id="${dropItemId}"]`);
-            event.currentTarget.appendChild(draggedElement);
+        if (isEmpty(draggedElement)) return;
 
-        } catch (e) {
-            console.error(e)
+        const dragItems = [...event.currentTarget.querySelectorAll('.drag-item')];
+        let isItemInserted = false;
+
+        for (const item of dragItems) {
+            const rect = item.getBoundingClientRect();
+            const cord = rect.y + rect.height / 2;
+
+            if (cord >= event.clientY) {
+                event.currentTarget.insertBefore(draggedElement, item);
+                isItemInserted = true;
+                break;
+            }
         }
+
+        if (!isItemInserted) event.currentTarget.appendChild(draggedElement);
     }
 }
