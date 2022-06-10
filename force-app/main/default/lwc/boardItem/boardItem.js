3fa8a4c607e963d32f1c30c1d7d3254a1beb0ea5
Vladyslav Kravchuk <vladyslav.kravchuk@customertimes.com>
2022-06-10 18:24:42 +0300

    Added Dropbox Integration

--- force-app/main/default/lwc/boardItem/boardItem.js
+++ force-app/main/default/lwc/boardItem/boardItem.js
@@ -2,6 +2,7 @@ import {LightningElement, api} from 'lwc';
 import {NavigationMixin} from 'lightning/navigation';
 
 import getActivityAttachments from '@salesforce/apex/KanbanBoardController.getActivityAttachments';
+import createAttachment from '@salesforce/apex/DropBoxApi.uploadFile';
 
 export default class BoardItem extends NavigationMixin(LightningElement) {
     @api item = {};
@@ -24,11 +25,30 @@ export default class BoardItem extends NavigationMixin(LightningElement) {
         });
     }
 
-    async addAttachment(event) {
+    addAttachment(event) {
+
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        console.log('FILEEEE__', file)
+
+        reader.onload = async () => {
+
+            let base64 = reader.result.split(',')[1]
+            const fileData = {
+                'filename': file.name,
+                'base64': base64,
+                'recordId': this.item.id
+            }
+            console.log(file, '__', fileData)
+            const res = await createAttachment({fileName: file.name, body: base64});
+            console.log('CREATED FILE', res);
+        }
+        reader.readAsDataURL(file)
+
         // console.log(JSON.stringify(this.item))
         // const res = await getActivityAttachments({activityId: this.item.id});
         // console.log("ATTACHMENTS__", JSON.stringify(res))
-        this.dispatchEvent(new CustomEvent('addattachment', {detail: {activityId: this.item.id}}))
+        // this.dispatchEvent(new CustomEvent('addattachment', {detail: {activityId: this.item.id}}))
     }
 
     async handleShowAttachmentModal(event) {
@@ -37,8 +57,7 @@ export default class BoardItem extends NavigationMixin(LightningElement) {
         //this.dispatchEvent(new CustomEvent('showattachment', {detail: {activityId: this.item.id}}))
         this.showModal = true;
         this.attachments = await getActivityAttachments({activityId: this.item.id});
-        console.log('ATTT____ ',this.attachments)
-        //await this.template.querySelector('c-attachment-modal')?.getAttachments(this.item.id);
+        console.log('ATTT____ ', this.attachments)
     }
 
     handleCloseAttachmentModal(event) {
